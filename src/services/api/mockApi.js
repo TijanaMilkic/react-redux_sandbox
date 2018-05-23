58a654e3d03375b0485dfb5b3fc2ae55b4913310
Nikola Milosevic <nikolamilosevic93@yahoo.com>
2018-05-23 16:54:06 +0200

    Mock api added

---
+++ src/services/api/mockApi.js
@@ -0,0 +1,115 @@
+import delay from './delay';
+
+const movies = [
+    {
+        "Title": "Dirk Gently's Holistic Detective Agency",
+        "Year": "2016–2017",
+        "Rated": "TV-14",
+        "Released": "22 Oct 2016",
+        "Runtime": "60 min",
+        "Genre": "Comedy, Mystery, Sci-Fi",
+        "Director": "N/A",
+        "Writer": "Max Landis",
+        "Actors": "Samuel Barnett, Elijah Wood, Hannah Marks, Jade Eshete",
+        "Plot": "Holistic detective Dirk Gently investigates cases involving the supernatural.",
+        "Language": "English",
+        "Country": "USA",
+        "Awards": "1 win & 2 nominations.",
+        "Poster": "https://ia.media-imdb.com/images/M/MV5BMTU3MDkzNzM5NF5BMl5BanBnXkFtZTgwMzEyNTgyMDI@._V1_SX300.jpg",
+        "Ratings": [
+          {
+            "Source": "Internet Movie Database",
+            "Value": "8.4/10"
+          }
+        ],
+        "Metascore": "N/A",
+        "imdbRating": "8.4",
+        "imdbVotes": "27,535",
+        "imdbID": "tt4047038",
+        "Type": "series",
+        "totalSeasons": "2",
+        "Response": "True"
+    },
+    {
+        "Title": "The Killing of a Sacred Deer",
+        "Year": "2017",
+        "Rated": "R",
+        "Released": "03 Nov 2017",
+        "Runtime": "121 min",
+        "Genre": "Drama, Mystery, Thriller",
+        "Director": "Yorgos Lanthimos",
+        "Writer": "Yorgos Lanthimos, Efthymis Filippou",
+        "Actors": "Barry G. Bernson, Herb Caillouet, Bill Camp, Raffey Cassidy",
+        "Plot": "Steven, a charismatic surgeon, is forced to make an unthinkable sacrifice after his life starts to fall apart, when the behavior of a teenage boy he has taken under his wing turns sinister.",
+        "Language": "English, French",
+        "Country": "UK, Ireland, USA",
+        "Awards": "2 wins & 30 nominations.",
+        "Poster": "https://ia.media-imdb.com/images/M/MV5BMjU4NDcwOTA2NF5BMl5BanBnXkFtZTgwMjE2OTg4MzI@._V1_SX300.jpg",
+        "Ratings": [
+            {
+            "Source": "Internet Movie Database",
+            "Value": "7.1/10"
+            },
+            {
+            "Source": "Rotten Tomatoes",
+            "Value": "80%"
+            },
+            {
+            "Source": "Metacritic",
+            "Value": "73/100"
+            }
+        ],
+        "Metascore": "73",
+        "imdbRating": "7.1",
+        "imdbVotes": "59,909",
+        "imdbID": "tt5715874",
+        "Type": "movie",
+        "DVD": "23 Jan 2018",
+        "BoxOffice": "$1,955,883",
+        "Production": "A24",
+        "Website": "http://killingofasacreddeer.movie/",
+        "Response": "True"
+    },
+    {
+        "Title": "The Square",
+        "Year": "2017",
+        "Rated": "R",
+        "Released": "25 Aug 2017",
+        "Runtime": "151 min",
+        "Genre": "Comedy, Drama",
+        "Director": "Ruben Östlund",
+        "Writer": "Ruben Östlund",
+        "Actors": "Claes Bang, Elisabeth Moss, Dominic West, Terry Notary",
+        "Plot": "A prestigious Stockholm museum's chief art curator finds himself in times of both professional and personal crisis as he attempts to set up a controversial new exhibit.",
+        "Language": "English, Swedish, Danish",
+        "Country": "Sweden, Germany, France, Denmark",
+        "Awards": "Nominated for 1 Oscar. Another 27 wins & 36 nominations.",
+        "Poster": "https://ia.media-imdb.com/images/M/MV5BMzc1MDY3NDIwMV5BMl5BanBnXkFtZTgwNzkwNzU0MzI@._V1_SX300.jpg",
+        "Ratings": [
+            {
+            "Source": "Internet Movie Database",
+            "Value": "7.3/10"
+            },
+            {
+            "Source": "Rotten Tomatoes",
+            "Value": "84%"
+            },
+            {
+            "Source": "Metacritic",
+            "Value": "73/100"
+            }
+        ],
+        "Metascore": "73",
+        "imdbRating": "7.3",
+        "imdbVotes": "31,450",
+        "imdbID": "tt4995790",
+        "Type": "movie",
+        "DVD": "30 Jan 2018",
+        "BoxOffice": "$1,284,672",
+        "Production": "Magnolia Pictures",
+        "Website": "http://www.Squarethefilm.com",
+        "Response": "True"
+    }
+];
+
+export default movies;
