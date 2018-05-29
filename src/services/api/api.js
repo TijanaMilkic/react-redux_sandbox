66bcef45a827e013aa17c1c0df8ce26080876c66
Nikola Milosevic <nikolamilosevic93@yahoo.com>
2018-05-29 15:40:18 +0200

    fetch movie method added

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,2 +1,12 @@
 const key = '169fcd3a';
 const baseURL = `http://www.omdbapi.com/?apikey=${key}&`;
+
+export default function fetchMovie(title, year) {
+    let url = `${baseURL}t=${title}`;
+    if (year) {
+        url += `&y=${year}`;
+    }
+    return fetch(url)
+        .then(response => response.json())
+}
+
