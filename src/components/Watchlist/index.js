199119ae11259b06e00318bdbc0a6a39b5c9ba37
Nikola Milosevic <nikolamilosevic93@yahoo.com>
2018-06-01 16:51:53 +0200

    filter logic added

--- src/components/Watchlist/index.js
+++ src/components/Watchlist/index.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import MoviesList from '../MoviesList';
+import MovieFilter from '../MovieFilter';
 
 function mapStateToProps(state, ownProps) {
+    const filterMovies = (filter, movies) => {
+        if (!filter.length) {
+            return [...movies]
+        }
+        return [
+            ...movies.filter(movie => {
+                const genreArr = movie.Genre.split(/, /);
+                for (let i = 0; i < genreArr.length; i++) {
+                  if (filter.indexOf(genreArr[i]) === -1) {
+                    return false;
+                  }
+                  return genreArr[i]
+                }
+                return movie
+            })
+        ]
+    }
+
     return {
-        movies: state.likes
+        movies: filterMovies([], state.likes)
     };
 }
 
 class Watchlist extends Component {
     render() {
         return (
-            <div>
+            <div className="watchlist">
                 <h1>Watchlist: </h1>
+                <MovieFilter />
                 <MoviesList movies={this.props.movies} />
             </div>
         );
